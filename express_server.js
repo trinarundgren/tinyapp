dcfdb876bef62a439f604268b94036877ed4f204
Trina <trina.rundgren@telus.com>
2022-12-06 19:20:45 -0800

    updated

--- express_server.js
+++ express_server.js
@@ -95,6 +95,13 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 });
 
+//LOGOUT
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
+
 // SERVER STUFF
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
