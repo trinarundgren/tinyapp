1a6811a91f264c271420d139a85bc9c67b7d0a05
Trina <trina.rundgren@telus.com>
2022-12-08 17:28:49 -0800

    updated

--- express_server.js
+++ express_server.js
@@ -5,8 +5,8 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bcrypt = require("bcryptjs");
 
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+const cookieSession = require('cookie-session');
+app.use(cookieSession({name: 'session', secret: 'UnicornPoop1973'}));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -44,7 +44,7 @@ const urlsForUser = (id) => {
 
 // URL INDEX
 app.get("/urls", (req, res) => {
-  const user_id = req.cookies['user_id'];
+  const user_id = req.session.user_id;
   const userURLS = urlsForUser(user_id);
   let templateVars = { urls: userURLS, user: users[user_id] };
   res.render("urls_index", templateVars);
@@ -55,15 +55,15 @@ app.post("/urls", (req, res) => {
   const shortUrl = generateRandomString();
   urlDatabase[shortUrl] = {
     longURL: req.body.longURL,
-    user_id: req.cookies['user_id']
+    userID: req.session.user_id
   };
   res.redirect(`/urls/${shortUrl}`);
 });
 
 // Create New URL - validates if user is logged in
 app.get('/urls/new', (req, res) => {
-  if (req.cookies['user_id']) {
-    const templateVars = {user: users[req.cookies['user_id']]};
+  if (req.session.user_id) {
+    const templateVars = {user: users[req.session.user_id]};
     res.render('urls_new', templateVars);
   } else {
     res.redirect('/login')
@@ -72,7 +72,7 @@ app.get('/urls/new', (req, res) => {
 
 // URL page showing short and long URL
 app.get("/urls/:shortURL", (req, res) => {
-  const user_id = req.cookies['user_id'];
+  const user_id = req.session.user_id;
   const userUrls = urlsForUser(user_id);
   let templateVars = { urls: userUrls, user: users[user_id], shortURL: req.params.shortURL };
   res.render("urls_show", templateVars);
@@ -81,7 +81,7 @@ app.get("/urls/:shortURL", (req, res) => {
 // validates if the url belongs to current user
 app.post('/urls/:shortUrl', (req, res) => {
   const shortURL = req.params.shortURL;
-  if (req.cookies[user_id] === urlDatabase[shortURL].user_id) {
+  if (req.session.user_id === urlDatabase[shortURL].user_id) {
     urlDatabase[shortURL].longURL = req.body.updatedURL;
   }
   res.redirect(`/urls`);
@@ -91,7 +91,7 @@ app.post('/urls/:shortUrl', (req, res) => {
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
 
-  if (req.cookies['user_id'] === urlDatabase[shortURL].user_id) {
+  if (req.session.user_id === urlDatabase[shortURL].user_id) {
     delete urlDatabase[shortURL];
   }
   
@@ -111,7 +111,7 @@ app.get('/u/:shortURL', (req, res) => {
 
 //LOGIN PAGE
 app.get('/login', (req, res) => {
-  let templateVars = {user: users[req.cookies['user_id']]};
+  let templateVars = {user: users[req.session.user_id]};
   res.render('urls_login', templateVars);
 });
 
@@ -120,37 +120,39 @@ app.post('/login', (req, res) => {
   const user = findUserWithEmailInDatabase(req.body.email, users);
 
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    req.session.userID = user.userID;
+    req.session.user_id = user.userID;
     res.redirect('/urls');
   } else {
     const errorMessage = 'Login credentials not valid. Please make sure you enter the correct username and password.';
-    res.status(401).render('urls_error', {user: users[req.session.userID], errorMessage});
+    res.status(401).render('urls_error', {user: users[req.session.user_id], errorMessage});
   }
 });
 
 //LOGOUT
 app.post('/logout', (req, res) => {
-  res.clearCookie('user_id');
+  res.clearCookie('session');
+  res.clearCookie('session.sig');
   res.redirect('/login');
 });
 
 // registration page
 app.get('/register', (req, res) => {
-  let templateVars = {user: users[req.cookies['user_id']]};
+  let templateVars = {user: users[req.session.user_id]};
   res.render('urls_registration', templateVars);
 });
 
 //REGISTER
+// register functionality
 app.post('/register', (req, res) => {
   if (req.body.email && req.body.password) {
     if (!findUserWithEmailInDatabase(req.body.email, users)) {
-      const user_id = generateRandomString();
-      users[user_id] = {
-        user_id,
+      const userID = generateRandomString();
+      users[userID] = {
+        userID,
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password, 10)
-      };
-      res.cookie('user_id', user_id);
+      }
+      req.session.user_id = userID;
       res.redirect('/urls');
     } else {
       res.statusCode = 400;
